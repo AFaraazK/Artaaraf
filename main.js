3347a745baae1450b466ba4406780c53f94ace42
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2024-01-12 22:51:35 -0500

    Drawing Functionality

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-const canvas = document.getElementById('draw_window')
-const toolbar = document.getElementById('toolbar');
-const context = canvas.getContext('2d');
-
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
+const canvas = document.getElementById('draw_window');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 let isDrawing = false;
 let lineWidth = 5;
-let startX;
-let startY;
 
-canvas.addEventListener('mousedown', e => {
-    alert();
-})
+const draw = (e) => {
+    if (!isDrawing) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+};
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+
+    const rect = canvas.getBoundingClientRect();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
 
